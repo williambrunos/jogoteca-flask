6ee905cca563390a5e2bf5d9abe876e72caa679d
William Bruno Sales de Paula Lima <william.lima@b3.com.br>
2024-09-24 15:32:39 -0300

    adicionando formulário para criação de novo jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
-    return render_template('lista.html')
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 
-app.run()
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'ps2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS5')
+jogos = [jogo1, jogo2, jogo3]
+
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=jogos)
+
+
+@app.route('/novo')
+def novo_jogo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+# TODO: Transformar esta rota /criar em middleware para que não fique carregando a lista apenas em /criar,
+#  deve ser ilustrada também no /
+@app.route('/criar', methods=['POST'])
+def criar_jogo():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    jogo = Jogo(nome, categoria, console)
+    jogos.append(jogo)
+
+    return render_template('lista.html', titulo="Jogos", jogos=jogos)
+
+
+app.run(debug=True)
